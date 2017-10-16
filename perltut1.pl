e5d2d758a9aebd0384b3011335531f0d0af6fbe0
lex-attax <lex.pavlovic@gmail.com>
2017-10-16 21:12:23 +0200

    Add files via upload

---
+++ perltut1.pl
@@ -0,0 +1,13 @@
+#youtube Tutorial: https://www.youtube.com/watch?v=WEghIXs8F6c
+
+use strict;
+use warnings;
+use diagnostics;
+
+use feature 'say';
+
+use feature "switch";
+
+use v5.16;
+
+print "Hello World\n";
